a0f5ad54bd3d3a81d61ec54925e49bf92fd85c37
zgs225 <zgs225@gmail.com>
2013-11-09 20:56:24 +0800

    Fix html entities issue

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,6 @@
 $(function($) {
   var article_content_block = $('.article-show .article-content');
+  var article_summary_block = $('.article-summary .article-summary-content');
 
   // 评论框
   $('input#content').focus(function() {
@@ -7,8 +8,25 @@ $(function($) {
   });
 
   // 过略符号
-  var content = $(article_content_block).html();
-  $(article_content_block).empty();
-  content = content.replace(/&lt;/g, '<').replace(/(&gt;)|( &gt; )/g, '>').replace(/&#39;/g, "'").replace(/&quot;/g, '"');
-  $(article_content_block).html(content);
+  if(article_content_block.length > 0) {
+    var content = $(article_content_block).html();
+    $(article_content_block).empty();
+    try {
+      content = content.replace(/&lt;/g, '<').replace(/(&gt;)|( &gt; )/g, '>').replace(/&#39;/g, "'").replace(/&quot;/g, '"');
+    } catch(e) {
+      console.log(e);
+    }
+    $(article_content_block).html(content);
+  }
+
+  if(article_summary_block.length > 0) {
+    var summary = $(article_summary_block).html();
+    $(article_summary_block).empty();
+    try {
+      summary = summary.replace(/&lt;/g, '<').replace(/(&gt;)|( &gt; )/g, '>').replace(/&#39;/g, "'").replace(/&quot;/g, '"');
+    } catch(e) {
+      console.log(e);
+    }
+    $(article_summary_block).html(summary);
+  }
 });
