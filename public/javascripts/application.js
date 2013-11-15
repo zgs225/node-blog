4c4d14859726a11d10e83f594a446009c28e7ff1
zgs225 <zgs225@gmail.com>
2013-11-15 21:56:53 +0800

    Show page syntax high light

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -57,5 +57,25 @@ $(function($) {
   $('.article-prev .cancel').click(function() {
     $('.article-prev').hide();
   });
+
+  // enable tab in textarea
+  $(document).delegate('#content', 'keydown', function(e) {
+    var keyCode = e.keyCode || e.which;
+
+    if (keyCode == 9) {
+      e.preventDefault();
+      var start = $(this).get(0).selectionStart;
+      var end = $(this).get(0).selectionEnd;
+
+      // set textarea value to: text before caret + tab + text after caret
+      $(this).val($(this).val().substring(0, start)
+          + "\t"
+          + $(this).val().substring(end));
+
+      // put caret at right position again
+      $(this).get(0).selectionStart =
+          $(this).get(0).selectionEnd = start + 1;
+    }
+  });
 });
 
